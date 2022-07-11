ba40cf7dc12a47a28ee9048ab7cb7d0f3a1d9d4e
Isaac Cheruiyot <isaacyegon05@gmail.com>
2022-07-11 08:15:11 -0700

    swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,16 +1,14 @@
 #include "main.h"
-
 /**
- * swap_int - use pointer to change value in memory
- * @a: pointer to int a location
- * @b: pointer to int b locstion
- * Return: Null/Void
- */
-void swap_int(int *a, int *b);
+* swap_int - Use pointer to change value in memory
+* @a: Pointer to int a location
+* @b: Pointer to int b location
+*Return: Null/Void
+*/
+void swap_int(int *a, int *b)
 {
-	int tmp;
-
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
+	int x = *a;
+	int y = *b;
+	*a = y;
+	*b = x;
 }
