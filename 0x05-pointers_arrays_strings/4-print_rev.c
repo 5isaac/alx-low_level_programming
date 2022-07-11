78ea95c4abca8df40d506b015b12cbca3868f846
Isaac Cheruiyot <isaacyegon05@gmail.com>
2022-07-11 09:44:27 -0700

    pointer to string location

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - Use a pointer to change the value of memory
+ * @s: pointer to s location
+ *Return: Null/Void
+ */
+void print_rev(char *s);
+{
+	int = 0;
+
+	while (*s)
+	{
+		i++;
+		s++;
+	}
+	s--;
+	for (; i > 0; i--)
+	{
+		putchar(*s);
+		s--;
+	}
+	putchar('\n');
+}
