bb599efdb23294444be41340d07aa8b05e7acaf0
Isaac Cheruiyot <isaacyegon05@gmail.com>
2022-07-13 11:16:00 -0700

    all fixed

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -31,14 +31,16 @@ void print_number(int n)
 			i /= 10;
 		}
 		mux = 1;
+
 		for (j = 1; j <= len; j++)
 			mux *= 10;
+
 		for (j = 0; j <= len; j++)
 		{
 			out = n / mux;
 			_putchar(out + '0');
 			n -= (mux * out);
-			mux /= 10;
+			mux / = 10;
 		}
 	}
 }
