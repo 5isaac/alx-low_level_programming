71d129cdca819e2c28989de9e6d7fe0f8e07932c
Isaac Cheruiyot <isaacyegon05@gmail.com>
2022-07-18 06:28:04 -0700

    hot fix

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,18 @@
 #include "main.h"
-
 /**
- * _memset - function n to fill the first bytes of memory
- * @s: pointer to char param
- * @b: data to change
- * @n: index
- * Return: *s
+ * _memset - fills n bytes of the memory pointed by s with the byte b
+ * @s: Pointer to array location
+ * @b: Constant byte to replace sotred date
+ * @n: number of bytes to be replaced
+ * Return: Pointer to memory s
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
-	unassigned int i;
+	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
 		*(s + i) = b;
 	}
-	return (s)
+	return (s);
 }
